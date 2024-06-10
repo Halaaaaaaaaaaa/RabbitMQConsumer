f2deb06c6ba7268b1bbe5c571ddbbdb5dbff9d08
Halaaaaaaaaaaa <heyshyun@gmail.com>
2024-06-10 17:03:42 +0900

    Spring AMQP를 이용한 Consumer 구현 ing_20240610

--- src/main/java/com/tisquare/rabbitmq/consumer/ConsumerService.java
+++ src/main/java/com/tisquare/rabbitmq/consumer/ConsumerService.java
@@ -1,14 +1,12 @@
 package com.tisquare.rabbitmq.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class ConsumerService {
@@ -16,19 +14,13 @@ public class ConsumerService {
     private static Logger logger = LoggerFactory.getLogger(ConsumerService.class);
 
     @RabbitListener(queues = "${spring.rabbitmq.queue.name}", containerFactory = "rabbitListenerContainerFactory")
-    public void receiveMessageFromQueue(LogMessage logMessage, @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
-        processMessage(logMessage, routingKey);
+    public void receiveMessageFromQueue(LogMessage logMessage) {
+        processMessage(logMessage);
     }
 
-    private void processMessage(LogMessage logMessage, String routingKey) {
+    private void processMessage(LogMessage logMessage) {
         try {
-            String[] routingKeyParts = routingKey.split("\\.");
-            String taskName = routingKeyParts[1];
-            String hostname = routingKeyParts[2];
-            logMessage.setHostname(hostname);
-            logMessage.setTaskname(taskName);
-
-            logger.info("RabbitMQ Consumer | 수신 성공 | [{}]", logMessage);
+            logger.info(logMessage.toString());
         } catch (ListenerExecutionFailedException e) {
             logger.error("RabbitMQ Consumer | 수신 실패 | convert 오류: [{}]", e.getMessage());
         } catch (Exception e) {
--- src/main/java/com/tisquare/rabbitmq/consumer/LogMessage.java
+++ src/main/java/com/tisquare/rabbitmq/consumer/LogMessage.java
@@ -2,17 +2,13 @@ package com.tisquare.rabbitmq.consumer;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 @Getter
 @Setter
@@ -20,83 +16,46 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class LogMessage implements Serializable {
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //TODO datetime, level, taskName, hostName, userId, msg
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime dateTime;
 
     private String level;
     private String taskname;
     private String hostname;
     private String userId;
-    private Long iuid;
     private String msg;
 
+    public  LogMessage() {}
 
-
-    public LogMessage() {}
-
-    public LogMessage(LocalDateTime dateTime, String level, String taskname, String hostname, String userId, Long iuid, String msg) {
+    public LogMessage(LocalDateTime dateTime, String level, String taskname, String hostname, String userId, String msg) {
         this.dateTime = dateTime;
         this.level = level;
         this.taskname = taskname;
         this.hostname = hostname;
         this.userId = userId;
-        this.iuid = iuid;
         this.msg = msg;
     }
 
-//    public byte[] toJsonBytes() {
-//        Map<String, Object> logData = new HashMap<>();
-//        logData.put("dateTime", dateTime);
-//        logData.put("userId", userId);
-//        logData.put("level", level);
-//        logData.put("msg", msg);
-//        logData.put("iuid", iuid);
-//        logData.put("hostname", hostname);
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        try {
-//            return mapper.writeValueAsBytes(logData);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Failed to convert log data to JSON", e);
-//        }
-//    }
+    // "[datetime] [level] [taskname] [hostname] [userId] - msg" 형식으로 파싱
+    public LogMessage(String logMessageStr) {
+        String[] parts = logMessageStr.split(" - ", 2);
+        String msg = parts[1];
+        String[] fields = parts[0].substring(1, parts[0].length() - 1).split("\\] \\[");
 
-//    private static final long serialVersionUID = 1L;
-//
-//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-//    private LocalDateTime dateTime;
-//
-//    private String userId;
-//    private String level;
-//    private String msg;
-//    private Long iuid;
-//    private String cp_code;
-//
-//    public LogMessage() {}
-//
-//    public LogMessage(LocalDateTime dateTime, String userId, String level, String msg, Long iuid, String cp_code) {
-//        this.dateTime = dateTime;
-//        this.userId = userId;
-//        this.level = level;
-//        this.msg = msg;
-//        this.iuid = iuid;
-//        this.cp_code = cp_code;
-//    }
-//
-//    public byte[] toJsonBytes() {
-//        Map<String, Object> logData = new HashMap<>();
-//        logData.put("dateTime", dateTime);
-//        logData.put("userId", userId);
-//        logData.put("level", level);
-//        logData.put("msg", msg);
-//        logData.put("iuid", iuid);
-//        logData.put("cp_code", cp_code);
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        try {
-//            return mapper.writeValueAsBytes(logData);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Failed to convert log data to JSON", e);
-//        }
-//    }
+        this.dateTime = LocalDateTime.parse(fields[0], DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+        this.level = fields[1];
+        this.taskname = fields[2];
+        this.hostname = fields[3];
+        this.userId = fields[4];
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] [%s] [%s] [%s] [%s] - %s",
+                dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),
+                level, taskname, hostname, userId, msg);
+    }
+
 }
